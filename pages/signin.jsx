8381c546314cca7c447a2838a99d278603049b56
hecthorl <hlvm03@gmail.com>
2021-09-20 15:34:20 -0500

    adding title

--- pages/signin.jsx
+++ pages/signin.jsx
@@ -3,6 +3,7 @@ import { useRouter } from "next/router";
 import { FcGoogle } from "react-icons/fc";
 import { getProviders, signIn } from "next-auth/client";
 import useUserAuth from "hooks/useUserAuth";
+import Head from "next/head";
 
 const SignIn = ({ providers }) => {
    const { isUser } = useUserAuth();
@@ -11,20 +12,25 @@ const SignIn = ({ providers }) => {
       if (isUser) replace("/");
    }, [isUser]);
    return (
-      <div className="flex flex-col justify-center items-center h-screen w-screen">
-         <h1 className="mb-5 text-3xl">Google meet - Clone</h1>
-         {Object.values(providers).map(provider => (
-            <div key={provider.name}>
-               <button
-                  className="flex text-xl items-center gap-x-2 border border-black hover:border-transparent hover:bg-black hover:text-white p-2 rounded transition-colors"
-                  onClick={() => signIn(provider.id)}
-               >
-                  <FcGoogle />
-                  <span>Inicia sesión con {provider.name}</span>
-               </button>
-            </div>
-         ))}
-      </div>
+      <>
+         <Head>
+            <title>SignIn</title>
+         </Head>
+         <div className="flex flex-col justify-center items-center h-screen w-screen">
+            <h1 className="mb-5 text-3xl">Google meet - Clone</h1>
+            {Object.values(providers).map(provider => (
+               <div key={provider.name}>
+                  <button
+                     className="flex text-xl items-center gap-x-2 border border-black hover:border-transparent hover:bg-black hover:text-white p-2 rounded transition-colors"
+                     onClick={() => signIn(provider.id)}
+                  >
+                     <FcGoogle />
+                     <span>Inicia sesión con {provider.name}</span>
+                  </button>
+               </div>
+            ))}
+         </div>
+      </>
    );
 };
 
